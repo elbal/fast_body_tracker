e86069c98deeb822765358e8258c1310d76ba8a5
Elia Balugani <elia.balugani@unimore.it>
2025-11-26 23:37:32 +0100

    Added dll binding to get device timestamp

--- pykinect_azure/k4a/image.py
+++ pykinect_azure/k4a/image.py
@@ -89,6 +89,9 @@ class Image:
 	def get_stride_bytes(self):
 		return int(_k4a.k4a_image_get_stride_bytes(self._handle))
 
+	def get_device_timestamp_usec(self) -> int:
+		return _k4a.k4a_image_get_device_timestamp_usec(self._handle)
+
 	def to_numpy(self):
 
 		if not self.is_valid():
