fb7ae1b54b7d24f0e016f7c0dfe28382037e0ac0
Elia Balugani <elia.balugani@unimore.it>
2025-12-09 20:01:45 +0100

    Added type hint to device

--- pykinect_azure/k4a/device.py
+++ pykinect_azure/k4a/device.py
@@ -4,13 +4,14 @@ from pykinect_azure.k4a import _k4a
 from pykinect_azure.k4a.capture import Capture
 from pykinect_azure.k4a.imu_sample import ImuSample
 from pykinect_azure.k4a.calibration import Calibration
+from pykinect_azure.k4a.configuration import Configuration
 from pykinect_azure.k4arecord.record import Record
 from pykinect_azure.k4a._k4atypes import K4A_WAIT_INFINITE
 
 
 class Device:
-	def __init__(self, index=0):
-		self._handle = self._get_handle(index)
+	def __init__(self, index: int = 0):
+		self._handle = self._create_handle(index)
 		self.serialnum = self._get_serialnum()
 		self.version = self._get_version()
 		self.configuration = None
@@ -27,7 +28,9 @@ class Device:
 	def handle(self):
 		return self._handle
 
-	def start(self, configuration, record=False, record_filepath="output.mkv"):
+	def start(
+			self, configuration: Configuration, record=False,
+			record_filepath="output.mkv"):
 		self.configuration = configuration
 		self._start_cameras(configuration)
 		self._start_imu()
@@ -36,7 +39,7 @@ class Device:
 				self._handle, self.configuration.handle(), record_filepath)
 			self.recording = True
 
-	def update(self, timeout_in_ms=K4A_WAIT_INFINITE):
+	def update(self, timeout_in_ms: int = K4A_WAIT_INFINITE) -> Capture:
 		# Get cameras capture
 		capture_handle = self._get_capture(timeout_in_ms)
 		capture = Capture(capture_handle, self.calibration)
@@ -45,7 +48,7 @@ class Device:
 
 		return capture
 
-	def update_imu(self, timeout_in_ms=K4A_WAIT_INFINITE):
+	def update_imu(self, timeout_in_ms: int = K4A_WAIT_INFINITE) -> ImuSample:
 		# Get imu sample
 		imu_sample_handle = self._get_imu_sample(timeout_in_ms)
 		imu_sample = ImuSample(imu_sample_handle)
@@ -53,11 +56,11 @@ class Device:
 		return imu_sample
 
 	@staticmethod
-	def device_get_installed_count():
+	def device_get_installed_count() -> int:
 		return int(_k4a.k4a_device_get_installed_count())
 
 	@staticmethod
-	def _get_handle(index=0):
+	def _create_handle(index: int) -> _k4a.k4a_device_t:
 		device_handle = _k4a.k4a_device_t()
 		result_code = _k4a.k4a_device_open(index, device_handle)
 		if result_code != _k4a.K4A_RESULT_SUCCEEDED:
@@ -65,7 +68,7 @@ class Device:
 
 		return device_handle
 
-	def _start_cameras(self, device_config):
+	def _start_cameras(self, device_config: Configuration):
 		self.calibration = self._get_calibration(
 			device_config.depth_mode, device_config.color_resolution)
 		result_code = _k4a.k4a_device_start_cameras(
@@ -84,7 +87,9 @@ class Device:
 	def _stop_imu(self):
 		_k4a.k4a_device_stop_imu(self._handle)
 
-	def _get_capture(self, timeout_in_ms=_k4a.K4A_WAIT_INFINITE):
+	def _get_capture(
+			self,
+			timeout_in_ms: int = _k4a.K4A_WAIT_INFINITE) -> _k4a.k4a_capture_t:
 		capture_handle = _k4a.k4a_capture_t()
 		result_code = _k4a.k4a_device_get_capture(
 			self._handle, capture_handle, timeout_in_ms)
@@ -93,7 +98,9 @@ class Device:
 
 		return capture_handle
 
-	def _get_imu_sample(self, timeout_in_ms=_k4a.K4A_WAIT_INFINITE):
+	def _get_imu_sample(
+			self,
+			timeout_in_ms: int = _k4a.K4A_WAIT_INFINITE) -> _k4a.k4a_imu_sample_t:
 		imu_sample_handle = _k4a.k4a_imu_sample_t()
 		result_code = _k4a.k4a_device_get_imu_sample(
 			self._handle, imu_sample_handle, timeout_in_ms)
@@ -102,9 +109,9 @@ class Device:
 
 		return imu_sample_handle
 
-	def _get_serialnum(self):
+	def _get_serialnum(self) -> str:
 		serial_number_size = ctypes.c_size_t()
-		result_code = _k4a.k4a_device_get_serialnum(
+		_ = _k4a.k4a_device_get_serialnum(
 			self._handle, None, serial_number_size)
 
 		serial_number = ctypes.create_string_buffer(
@@ -116,7 +123,8 @@ class Device:
 
 		return serial_number.value.decode("utf-8")
 
-	def _get_calibration(self, depth_mode, color_resolution):
+	def _get_calibration(
+			self, depth_mode: int, color_resolution: int) -> Calibration:
 		calibration_handle = _k4a.k4a_calibration_t()
 		result_code = _k4a.k4a_device_get_calibration(
 			self._handle, depth_mode, color_resolution, calibration_handle)
@@ -125,7 +133,7 @@ class Device:
 
 		return Calibration(calibration_handle)
 
-	def _get_version(self):
+	def _get_version(self) -> _k4a.k4a_hardware_version_t:
 		version = _k4a.k4a_hardware_version_t()
 		result_code = _k4a.k4a_device_get_version(self._handle, version)
 		if result_code != _k4a.K4A_RESULT_SUCCEEDED:
