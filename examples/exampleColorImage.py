57383ef92701ffdd446bfe967946819e02e1855d
Elia Balugani <elia.balugani@unimore.it>
2025-12-11 21:35:26 +0100

    Added frame counter to color image example

--- examples/exampleColorImage.py
+++ examples/exampleColorImage.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 import pykinect_azure as pykinect
 
@@ -17,10 +18,22 @@ def main():
 
 	device = pykinect.start_device(config=device_config)
 	cv2.namedWindow("Color Image", cv2.WINDOW_NORMAL)
+	FRAME_WINDOW = 600
+	frame_count = 0
+	start_time = time.perf_counter()
 	while True:
+		frame_count += 1
 		capture = device.update()
 		color_image = capture.get_color_image()
 		cv2.imshow("Color Image", color_image)
+
+		if frame_count >= FRAME_WINDOW:
+			end_time = time.perf_counter()
+			elapsed_time = end_time - start_time
+			fps = frame_count / elapsed_time
+			print(f"FPS: {fps:.2f}")
+			start_time = time.perf_counter()
+			frame_count = 0
 
 		# Press q key to stop.
 		if cv2.waitKey(1) == ord("q"):
