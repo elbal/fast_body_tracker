ef20604bb5d249d0fff3fc5b5b93dbffb10951bc
Elia Balugani <elia.balugani@unimore.it>
2026-01-13 00:42:59 +0100

    Moved scripts for internal use to a dedicated folder

--- src/fast_body_tracker/calibration/charuco_board.py
+++ src/fast_body_tracker/calibration/charuco_board.py
@@ -3,24 +3,14 @@ import numpy as np
 
 
 def save_charuco_board(path: str):
-    """
-    Save a printable (3x3) A3 ChArUco board image.
-
-    Parameters
-    ----------
-    path : str
-        Path to save the ChArUco board image.
-    """
-
-    # A3 dimensions (mm).
     a3_width_mm = 297
     a3_height_mm = 420
     dpi = 300
-    # 3x3 configuration.
+
     n_squares_w = 3
     n_squares_h = 3
-    square_length_mm = 125  # large squares.
-    marker_length_mm = 95  # large markers.
+    square_length_mm = 94.0
+    marker_length_mm = 94.0 * 0.76
 
     aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_1000)
     board = cv2.aruco.CharucoBoard(
@@ -29,19 +19,15 @@ def save_charuco_board(path: str):
 
     pixel_w = int(a3_width_mm / 25.4 * dpi)
     pixel_h = int(a3_height_mm / 25.4 * dpi)
-    pixel_margin = 20
+
     image = board.generateImage(
-        outSize=(pixel_w - 2 * pixel_margin, pixel_h - 2 * pixel_margin),
-        marginSize=pixel_margin, borderBits=1)
-    # Center the board on a white canvas.
-    canvas = np.ones((pixel_h, pixel_w), dtype=image.dtype) * 255
+        outSize=(pixel_w - 100, pixel_h - 100), marginSize=20, borderBits=1)
+
+    canvas = np.ones((pixel_h, pixel_w), dtype=np.uint8) * 255
     h, w = image.shape[:2]
-    y0 = (pixel_h-h) // 2
-    x0 = (pixel_w-w) // 2
-    canvas[y0:y0+h, x0:x0+w] = image
+    y0 = (pixel_h - h) // 2
+    x0 = (pixel_w - w) // 2
+    canvas[y0:y0 + h, x0:x0 + w] = image
 
-    cv2.imwrite(path + "charuco_board_A3_3x3.png", canvas)
-
-
-if __name__ == "__main__":
-    save_charuco_board("../../data/charuco_boards/")
+    output_file = f"{path}charuco_board_A3_3x3.png"
+    cv2.imwrite(output_file, canvas)
