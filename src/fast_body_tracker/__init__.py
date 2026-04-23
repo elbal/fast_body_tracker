f2e1da0a88bd4e5d4f6a1fe8e306a1ea241b8943
Elia Balugani <elia.balugani@unimore.it>
2026-04-24 00:53:46 +0200

    Initialization fixed

--- src/fast_body_tracker/__init__.py
+++ src/fast_body_tracker/__init__.py
@@ -5,10 +5,10 @@ from .initializer import (
     start_playback as start_playback,
 )
 from .data_capture_pipeline import (
-    body_saver_thread as body_saver_thread,
     capture_thread as capture_thread,
     computation_thread as computation_thread,
     default_pipeline as default_pipeline,
+    saver_thread as saver_thread,
     unification_thread as unification_thread,
     video_saver_thread as video_saver_thread,
     visualization_main_tread as visualization_main_tread,
